4d7aea30454437169cf6d4f05e57193a6a193d26
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-07-26 04:13:42 +0900

    temporary commit [ci skip]

--- make_crops.py
+++ make_crops.py
@@ -1,5 +1,5 @@
 import os, shutil, cv2, numpy as np
-def write_cropped_im(dpath, fname, size, crop_size, crop_index, crop_loc, cropped_im, empty_index):
+def write_cropped_im(dpath, fname, size, crop_size, crop_index, crop_loc, cropped_im, ngemptyi):
     fnamewox = fname[:-4]
     cropped_fname = f'{fnamewox}_{crop_index[0]}_{crop_index[1]}.jpg'
     tfpath = f'{dpath}/all/{fnamewox}.txt'
@@ -7,6 +7,7 @@ def write_cropped_im(dpath, fname, size, crop_size, crop_index, crop_loc, croppe
     with open(tfpath) as f:
         tdata = [[int(y) if i == 0 else float(y) for i, y in enumerate(x.split(' '))] for x in f.read().strip().split('\n')]
     cropped_tdata = []
+    gray = False
     for td in tdata:
         loc = [int(round(size[i] * (td[2 - i] - td[4 - i] / 2))) for i in range(2)]
         loc_size = [int(round(size[i] * td[4 - i])) for i in range(2)]
@@ -25,6 +26,7 @@ def write_cropped_im(dpath, fname, size, crop_size, crop_index, crop_loc, croppe
         if any([cropped_max[i] - cropped_loc[i] != loc_size[i] for i in range(2)]):
             print(f'gray zone rendered on {crop_index}')
             cropped_im[cropped_loc[0]:cropped_max[0], cropped_loc[1]:cropped_max[1]] = 127
+            gray = True
             continue
         cropped_center_p = [(cropped_loc[i] + cropped_max[i]) / 2 / crop_size[i] for i in range(2)]
         loc_size_p = [loc_size[i] / crop_size[i] for i in range(2)]
@@ -35,15 +37,17 @@ def write_cropped_im(dpath, fname, size, crop_size, crop_index, crop_loc, croppe
         ]])
     empty = len(cropped_tdata) == 0
     written = False
-    # annotationが無い場合は、10回に1回のみ保存する
-    if not empty or empty_index % 10 == 0:
+    # not grayでかつannotationが無い場合は、100回に1回のみ保存する。grayは保存しない
+    if not gray and (not empty or ngemptyi % 100 == 0):
         cropped_tfpath = f'{dpath}/cropped/{fnamewox}_{crop_index[0]}_{crop_index[1]}.txt'
         with open(cropped_tfpath, 'w') as f:
             f.write('\n'.join([' '.join(x) for x in cropped_tdata]))
         cropped_fpath = f'{dpath}/cropped/{fnamewox}_{crop_index[0]}_{crop_index[1]}.jpg'
         cv2.imwrite(cropped_fpath, cropped_im)
         written = True
-    return [empty, written]
+    if not gray and empty:
+        ngemptyi += 1
+    return [empty, gray, written, ngemptyi]
 dpath = os.path.dirname(os.path.realpath(__file__))
 shutil.rmtree(f'{dpath}/cropped', ignore_errors=True)
 os.makedirs(f'{dpath}/cropped')
@@ -51,7 +55,7 @@ crop_size = [640, 480]
 overlap_size = [320, 240]
 jpg_names = [p for p in os.listdir(f'{dpath}/all') if p[-4:] == '.jpg']
 os.system(f'cp {dpath}/all/classes.txt {dpath}/cropped/')
-emptyi = 0
+ngemptyi = 0
 emptyc = 0
 totalc = 0
 for ji, p in enumerate(jpg_names):
@@ -72,11 +76,9 @@ for ji, p in enumerate(jpg_names):
             max_size = [min([crop_index[k] * overlap_size[k] + crop_size[k], size[k]]) for k in range(2)]
             min_size = [max_size[k] - crop_size[k] for k in range(2)]
             cropped_im = np.copy(im[min_size[0]:max_size[0], min_size[1]:max_size[1]])
-            empty_data, written = write_cropped_im(dpath, p, size, crop_size, crop_index, min_size, cropped_im, emptyi)
-            if empty_data:
-                emptyi += 1
+            empty, gray, written, ngemptyi = write_cropped_im(dpath, p, size, crop_size, crop_index, min_size, cropped_im, ngemptyi)
             if written:
                 totalc += 1
-                if empty_data:
+                if empty:
                     emptyc += 1
 print(f'empty count: {emptyc}, total count: {totalc}')
